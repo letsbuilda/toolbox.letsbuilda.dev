f2a70b2170f401aaec896751353f19d5a289135f
Robin5605 <robinjefferson123@gmail.com>
2023-03-12 00:08:55 -0600

    Remove localhost as base URL

--- src/common/api.ts
+++ src/common/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "http://api.letsbuilda.dev";
 
 export interface GenerateRandomNumberAPIResponse {
     numbers: number[],
